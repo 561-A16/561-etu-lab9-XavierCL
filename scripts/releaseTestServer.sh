9afe5cebf106171f8833acc214d0a7b065165af2
xaviercl <xaviercl>
2016-10-29 16:58:25 -0400

    Fix script and env travis variables to stop containers in test server before starting the new one

--- scripts/releaseTestServer.sh
+++ scripts/releaseTestServer.sh
@@ -1,4 +1,7 @@
 #!bin/bash
 set -e
+docker rmi xaviercl/webapp:latest
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 docker load -i $HOME/webapp.tar
-docker run -it -p 8080:5000 xaviercl/webapp:latest
+docker run -it -p 8099:5000 xaviercl/webapp:latest
