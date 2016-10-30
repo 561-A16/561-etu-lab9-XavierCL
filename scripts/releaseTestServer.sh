d4e4ffae585856cad1860ccc40f3ac4ccd9f8253
xaviercl <xaviercl>
2016-10-30 00:07:07 -0400

    add verbose and detached release test server script

--- scripts/releaseTestServer.sh
+++ scripts/releaseTestServer.sh
@@ -1,6 +1,7 @@
 #!bin/bash
+set -v
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi xaviercl/webapp:latest
 docker pull xaviercl/webapp:latest
-docker run -it -p 8099:5000 xaviercl/webapp:latest
+docker run -itd -p 8099:5000 xaviercl/webapp:latest
