115b08ca478a9ab095d066695388c41784241440
xaviercl <xaviercl>
2016-10-29 23:55:50 -0400

    fix release test server commands order

--- scripts/releaseTestServer.sh
+++ scripts/releaseTestServer.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-docker rmi xaviercl/webapp:latest
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+docker rmi xaviercl/webapp:latest
 docker pull xaviercl/webapp:latest
 docker run -it -p 8099:5000 xaviercl/webapp:latest
